9675c6c73570a7855d01873811fa9dfcf271d3a0
basita512 <mail2basita@gmail.com>
2024-08-30 16:41:52 +0530

    made Add Tasks

--- ToDo App/src/App.jsx
+++ ToDo App/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,9 +10,11 @@ function App() {
     setTask(event.target.value)
   }
 
+
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value)
   }
+
 
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent page reload after submission
@@ -29,6 +30,8 @@ function App() {
     <>
       <div className="container">
 
+        <h1 class="heading">ToDo App</h1>
+
         <input 
           type="text" 
           value={task} 
@@ -41,13 +44,19 @@ function App() {
           onChange={handleDescriptionChange} 
           placeholder='Enter Description' />
 
-      </div>
-        
-      <button onClick={handleSubmit}>Sumbit</button>
+        <button onClick={handleSubmit}>Sumbit</button>
 
-      <div className="ToDo-container">
-        <h3>Tasks List</h3>
-
+        <div className="ToDo-container">
+          <h3>Tasks List</h3>
+          <ul>
+            {taskList.map((event, index) => (
+              <li key={index}>
+                <strong>Task : </strong> {event.task} <br />
+                <strong>Description : </strong> {event.description}
+              </li>
+            ))}
+          </ul>
+        </div>
 
       </div>
 
